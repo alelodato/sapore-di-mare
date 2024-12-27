9d0290ff2403e7431fcac2c61bba0dceee526aa7
Alessio Lodato <alss.ldt@gmail.com>
2024-12-27 17:04:26 +0000

    renamed procfile and set debug to true

--- saporedm/settings.py
+++ saporedm/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pm5)02i$*a#9rzl&-o4(84e)%@g9g&n!76k6v@td0=zu5(ohnf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-alelodato-saporedimare-sv7515qd9kz.ws-eu117.gitpod.io',
