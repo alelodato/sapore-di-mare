b2d73098ef42fd9b9017c143ad5eb39c9ddb7831
Alessio Lodato <alss.ldt@gmail.com>
2024-12-29 12:59:36 +0000

    created html files for apps

--- saporedm/settings.py
+++ saporedm/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-alelodato-saporedimare-sv7515qd9kz.ws-eu117.gitpod.io',
