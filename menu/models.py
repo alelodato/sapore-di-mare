4ef35ad8fc4be250fceed188aafeeb202e1cc25d
Alessio Lodato <alss.ldt@gmail.com>
2025-01-02 23:11:16 +0000

    created menu detail file to display individual menus

--- menu/models.py
+++ menu/models.py
@@ -6,3 +6,4 @@ class Menu(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     content = models.TextField()
+    excerpt = models.TextField(blank=True)
--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Menu
 # Create your views here.
@@ -6,3 +6,18 @@ from .models import Menu
 class MenuList(generic.ListView):
     queryset = Menu.objects.all()
     template_name = "menu/menu.html"
+    paginate_by = 2
+
+def menu_detail(request, slug):
+    """
+    Display an individual :model:`menu.Menu`.
+    """
+
+    queryset = Menu.objects.all()
+    menu = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "menu/menu_detail.html",
+        {"menu": menu},
+    )
