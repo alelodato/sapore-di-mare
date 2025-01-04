1b85da7348e3d8c8dc5bbe33248b032e74cf5277
Alessio Lodato <alss.ldt@gmail.com>
2025-01-04 18:10:17 +0000

    created datepicker

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+# class Table(models.Model):
+#     seats = models.IntegerField()
+#     date = models.DateField(auto_now_add=True)
+#     time = models.TimeField(auto_now_add=True)
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from django.views import generic
+# from .models import Table
 # Create your views here.
+class Bookings(generic.ListView):
+    queryset = "booking.html"
+    template_name = "booking/booking.html"
+#class Bookings(generic.ListView):
+#    queryset = Table.objects.all()
+#    template_name = "booking/booking.html"
+
+# def booking_detail(request):
+
+#    queryset = Review.objects.all()
+#    booking = get_object_or_404(queryset,)
+
+#    return render(
+#        request,
+#        "booking/booking.html",
+#        {"booking_system": booking_system},
+#    )
