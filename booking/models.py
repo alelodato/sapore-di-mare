3ed43a9268f140e5f44cd929717f32633bab59de
Alessio Lodato <alss.ldt@gmail.com>
2025-01-06 14:15:35 +0000

    created table model for booking page

--- booking/models.py
+++ booking/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-# class Table(models.Model):
-#     seats = models.IntegerField()
-#     date = models.DateField(auto_now_add=True)
-#     time = models.TimeField(auto_now_add=True)
+class Table(models.Model):
+     seats = models.IntegerField()
+     time = models.TimeField(auto_now_add=True)
--- booking/views.py
+++ booking/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-# from .models import Table
+from .models import Table
 # Create your views here.
+
 class Bookings(generic.ListView):
-    queryset = "booking.html"
+    queryset = Table.objects.all()
     template_name = "booking/booking.html"
-#class Bookings(generic.ListView):
-#    queryset = Table.objects.all()
-#    template_name = "booking/booking.html"
 
-# def booking_detail(request):
+def booking_detail(request):
 
-#    queryset = Review.objects.all()
-#    booking = get_object_or_404(queryset,)
+    queryset = Review.objects.all()
+    booking = get_object_or_404(queryset,)
 
-#    return render(
-#        request,
-#        "booking/booking.html",
-#        {"booking_system": booking_system},
-#    )
+    return render(
+        request,
+        "booking/booking.html",
+        {"booking": booking },
+    )
