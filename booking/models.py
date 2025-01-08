bac6a7b5e22b11894b6d5c66d7def9666a9a7cac
Alessio Lodato <alss.ldt@gmail.com>
2025-01-08 13:27:45 +0000

    created table model for restaurant bookings

--- booking/models.py
+++ booking/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from datetime import datetime
 from django.contrib.auth.models import User
 # Create your models here.
 
 class Table(models.Model):
+     customer = models.CharField(max_length=50, default="customer full name")
      seats = models.IntegerField()
-     time = models.DateField()
+     date = models.DateField(default=datetime.now)
+     time = models.TimeField(default=datetime.now)
