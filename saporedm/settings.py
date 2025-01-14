67389beb05d48b9d2efc5248ad7c2ff319e7a474
Alessio Lodato <alss.ldt@gmail.com>
2025-01-14 14:01:01 +0000

    collected static files and set debug to false for heroku deployment

--- saporedm/settings.py
+++ saporedm/settings.py
@@ -26,10 +26,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-alelodato-saporedimare-qi1x87s5nxu.ws-eu117.gitpod.io',
+    '8000-alelodato-saporedimare-x6ivobubbgp.ws.codeinstitute-ide.net',
     '.herokuapp.com'
 ]
 
@@ -110,7 +110,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.gitpod.io",
+    "https://*.codeinstitute-ide.net",
     "https://*.herokuapp.com"
 ]
 
