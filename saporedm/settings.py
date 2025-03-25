937d1a88336a7efa75cf43a7786159bfd0bf5d6d
alelodato <alss.ldt@gmail.com>
2025-03-25 13:07:26 +0000

    migrated project to vscode

--- saporedm/settings.py
+++ saporedm/settings.py
@@ -26,10 +26,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-alelodato-saporedimare-x6ivobubbgp.ws.codeinstitute-ide.net',
+    '127.0.0.1',
     '.herokuapp.com'
 ]
 
@@ -104,7 +104,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeinstitute-ide.net",
+    "https://localhost",
     "https://*.herokuapp.com"
 ]
 
