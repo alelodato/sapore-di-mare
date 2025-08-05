82c9dbe905d17a54fb42e9f73222460cc0fcddfa
alelodato <alss.ldt@gmail.com>
2025-08-05 11:50:43 +0200

    added x frame options to create responsive mockup with am i responsive

--- saporedm/settings.py
+++ saporedm/settings.py
@@ -152,3 +152,5 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
